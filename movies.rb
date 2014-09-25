9295ca79c13fb3d348bb61c1c345e3c336aaf75a
Jeff Cohen <cohen.jeff@gmail.com>
2014-09-25 10:27:05 -0500

    First draft of enumerables challenge

---
+++ movies.rb
@@ -0,0 +1,44 @@
+movies = [ { :title => "Apollo 13",
+             :year => 1995,
+             :reviews => 10,
+             :plot => "Three astronauts must devise a strategy to return to Earth safely after their spacecraft undergoes massive internal damage.",
+             :image_url => "http://ia.media-imdb.com/images/M/MV5BMTM2Njg2NjU5NF5BMl5BanBnXkFtZTYwODI5MDc4._V1_SY226_SX144_.jpg"
+          },
+          { :title => "Lincoln",
+            :year => 2012,
+            :reviews => 6,
+            :plot => "As the Civil War continues to rage, America's president struggles with continuing carnage on the battlefield and as he fights with many inside his own cabinet on the decision to emancipate the slaves.",
+            :image_url => "http://ia.media-imdb.com/images/M/MV5BMTQzNzczMDUyNV5BMl5BanBnXkFtZTcwNjM2ODEzOA@@._V1_SY317_CR0,0,214,317_.jpg"
+          },
+          { :title => "Star Wars",
+            :year => 1977,
+            :reviews => 3,
+            :plot => "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the universe from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.",
+            :image_url => "http://ia.media-imdb.com/images/M/MV5BMTU4NTczODkwM15BMl5BanBnXkFtZTcwMzEyMTIyMw@@._V1_SX214_.jpg"
+          },
+          { :title => "Raiders of the Lost Ark",
+            :year => 1981,
+            :reviews => 7,
+            :plot => "Archeologist and adventurer Indiana Jones is hired by the US government to find the Ark of the Covenant before the Nazis.",
+            :image_url => "http://ia.media-imdb.com/images/M/MV5BMjA0ODEzMTc1Nl5BMl5BanBnXkFtZTcwODM2MjAxNA@@._V1_SX214_.jpg"
+          },
+          { :title => "Backdraft",
+            :year => 1991,
+            :reviews => 8,
+            :plot => "Two Chicago firefighter brothers who don't get along have to work together while a dangerous arsonist is on the loose.",
+            :image_url => "http://ia.media-imdb.com/images/M/MV5BMTY2NTI5ODIwOV5BMl5BanBnXkFtZTcwMDI2ODMzMQ@@._V1_SY317_CR6,0,214,317_.jpg"
+          }
+      ]
+
+
+# Replace the hardcoded answers with Ruby code
+# Hint: collect, detect, select, reject, inject, sort, count, size, length
+
+total_number_of_movies = 5
+movies_before_1990 = 2
+total_number_of_reviews = 34
+titles_in_order = "Apollo 13, Backdraft, Lincoln, Raiders of the Lost Ark, Star Wars"
+has_movie_with_the_word_spacecraft = true
+
+
+
